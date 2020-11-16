89b20855535d10cbcfa300c8b46b84001e1aea9b
Matt Kaiser <matt@kaiserengineering.io>
2020-11-16 00:15:52 -0500

    Require support for each PID

--- src/lib_obdii.c
+++ src/lib_obdii.c
@@ -324,38 +324,60 @@ static uint8_t lookup_payload_length( uint8_t mode, uint16_t PID )
         case MODE1:
             switch ( PID )
             {
+                #ifdef MODE1_CALCULATED_ENGINE_LOAD_VALUE_SUPPORTED
                 case MODE1_CALCULATED_ENGINE_LOAD_VALUE:
                     return MODE1_CALCULATED_ENGINE_LOAD_VALUE_LEN;
+                #endif
 
+                #ifdef MODE1_ENGINE_COOLANT_TEMPERATURE_SUPPORTED
                 case MODE1_ENGINE_COOLANT_TEMPERATURE:
                     return MODE1_ENGINE_COOLANT_TEMPERATURE_LEN;
+                #endif
 
+                #ifdef MODE1_ENGINE_RPM_SUPPORTED
                 case MODE1_ENGINE_RPM:
                     return MODE1_ENGINE_RPM_LEN;
+                #endif
 
+                #ifdef MODE1_INTAKE_MANIFOLD_ABSOLUTE_PRESSURE_SUPPORTED
                 case MODE1_INTAKE_MANIFOLD_ABSOLUTE_PRESSURE:
                     return MODE1_INTAKE_MANIFOLD_ABSOLUTE_PRESSURE_LEN;
+                #endif
 
+                #ifdef MODE1_VEHICLE_SPEED_SUPPORTED
                 case MODE1_VEHICLE_SPEED:
                     return MODE1_VEHICLE_SPEED_LEN;
+                #endif
 
+                #ifdef MODE1_INTAKE_AIR_TEMPERATURE_SUPPORTED
                 case MODE1_INTAKE_AIR_TEMPERATURE:
                     return MODE1_INTAKE_AIR_TEMPERATURE_LEN;
+                #endif
 
+                #ifdef MODE1_MAF_AIR_FLOW_RATE_SUPPORTED
                 case MODE1_MAF_AIR_FLOW_RATE:
                     return MODE1_MAF_AIR_FLOW_RATE_LEN;
+                #endif
 
+                #ifdef MODE1_THROTTLE_POSITION_SUPPORTED
                 case MODE1_THROTTLE_POSITION:
                     return MODE1_THROTTLE_POSITION_LEN;
+                #endif
 
+                #ifdef MODE1_BAROMETRIC_PRESSURE_SUPPORTED
                 case MODE1_BAROMETRIC_PRESSURE:
                     return MODE1_BAROMETRIC_PRESSURE_LEN;
+                #endif
 
+                #ifdef MODE1_ABSOLUTE_LOAD_VALUE_SUPPORTED
                 case MODE1_ABSOLUTE_LOAD_VALUE:
                     return MODE1_ABSOLUTE_LOAD_VALUE_LEN;
+                #endif
 
+                #ifdef MODE1_AMBIENT_AIR_TEMPERATURE_SUPPORTED
                 case MODE1_AMBIENT_AIR_TEMPERATURE:
                     return MODE1_AMBIENT_AIR_TEMPERATURE_LEN;
+                #endif
 
                 default:
                     return 0x00;
@@ -365,11 +387,15 @@ static uint8_t lookup_payload_length( uint8_t mode, uint16_t PID )
         case MODE22:
             switch ( PID )
             {
+                #ifdef MODE22_INTAKE_AIR_TEMPERATURE_SUPPORTED
                 case MODE22_INTAKE_AIR_TEMPERATURE:
                     return MODE22_INTAKE_AIR_TEMPERATURE_LEN;
+                #endif
 
+                #ifdef MODE22_CHARGE_AIR_TEMPERATURE_SUPPORTED
                 case MODE22_CHARGE_AIR_TEMPERATURE:
                     return MODE22_CHARGE_AIR_TEMPERATURE_LEN;
+                #endif
 
                 default:
                     return 0;
@@ -500,29 +526,93 @@ static float get_pid_value( uint8_t mode, uint16_t pid, uint8_t data[] )
             switch( pid )
             {
                 /*    Equation: (A * 100) / 255    */
+                #ifdef MODE1_CALCULATED_ENGINE_LOAD_VALUE_SUPPORTED
+                    #ifndef EQ_100_TIMES_A_OVER_255
+                    #define EQ_100_TIMES_A_OVER_255
+                    #endif
                 case MODE1_CALCULATED_ENGINE_LOAD_VALUE:
+                #endif
+
+                #ifdef EQ_100_TIMES_A_OVER_255
                     return (((float)data[A]) * (float)100) / (float)255;
                     break;
+                #endif
 
+                #ifdef MODE1_INTAKE_AIR_TEMPERATURE_SUPPORTED
+                    #ifndef EQ_A_MINUS_40
+                    #define EQ_A_MINUS_40
+                    #endif
                 case MODE1_INTAKE_AIR_TEMPERATURE:
+                #endif
+
+                #ifdef MODE1_ENGINE_COOLANT_TEMPERATURE_SUPPORTED
+                    #ifndef EQ_A_MINUS_40
+                    #define EQ_A_MINUS_40
+                    #endif
                 case MODE1_ENGINE_COOLANT_TEMPERATURE:
+                #endif
+
+                #ifdef MODE1_AMBIENT_AIR_TEMPERATURE_SUPPORTED
+                    #ifndef EQ_A_MINUS_40
+                    #define EQ_A_MINUS_40
+                    #endif
                 case MODE1_AMBIENT_AIR_TEMPERATURE:
+                #endif
+
+                #ifdef EQ_A_MINUS_40
                     return ((float)data[A] - (float)40);
                     break;
+                #endif
 
+                #ifdef MODE1_ENGINE_RPM_SUPPORTED
+                    #ifndef EQ_256_TIMES_A_PLUS_B_OVER_255
+                    #define EQ_256_TIMES_A_PLUS_B_OVER_255
+                    #endif
                 case MODE1_ENGINE_RPM:
+                #endif
+
+                #ifdef EQ_256_TIMES_A_PLUS_B_OVER_255
                     return (((float)256 * (float)data[A] ) + (float)data[B] ) / (float)4;
                     break;
+                #endif
 
+                #ifdef MODE1_INTAKE_MANIFOLD_ABSOLUTE_PRESSURE_SUPPORTED
+                    #ifndef EQ_A
+                    #define EQ_A
+                    #endif
                 case MODE1_INTAKE_MANIFOLD_ABSOLUTE_PRESSURE:
+                #endif
+
+                #ifdef MODE1_VEHICLE_SPEED_SUPPORTED
+                    #ifndef EQ_A
+                    #define EQ_A
+                    #endif
                 case MODE1_VEHICLE_SPEED:
+                #endif
+
+                #ifdef MODE1_BAROMETRIC_PRESSURE_SUPPORTED
+                    #ifndef EQ_A
+                    #define EQ_A
+                    #endif
                 case MODE1_BAROMETRIC_PRESSURE:
+                #endif
+
+                #ifdef EQ_A
                     return (float)data[A];
                     break;
+                #endif
 
+                #ifdef MODE1_MAF_AIR_FLOW_RATE_SUPPORTED
+                    #ifndef EQ_256_TIMES_A_PLUS_B_OVER_100
+                    #define EQ_256_TIMES_A_PLUS_B_OVER_100
+                    #endif
                 case MODE1_MAF_AIR_FLOW_RATE:
+                #endif
+
+                #ifdef EQ_256_TIMES_A_PLUS_B_OVER_100
                     return (((float)256 * (float)data[A] ) + (float)data[B] ) / (float)100;
                     break;
+                #endif
 
                 default:
                     return -1;
@@ -532,12 +622,16 @@ static float get_pid_value( uint8_t mode, uint16_t pid, uint8_t data[] )
         case MODE22:
             switch( pid )
             {
+                #ifdef MODE22_CHARGE_AIR_TEMPERATURE_SUPPORTED
                 case MODE22_CHARGE_AIR_TEMPERATURE:
                     return (((float)256 * (float)data[A] ) + (float)data[B] ) / (float)64;
+                #endif
 
+                #ifdef MODE22_INTAKE_AIR_TEMPERATURE_SUPPORTED
                 case MODE22_INTAKE_AIR_TEMPERATURE:
                     return ((float)data[A] - (float)40);
                     break;
+                #endif
 
                 default:
                     return -1;
